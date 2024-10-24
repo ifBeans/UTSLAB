a7723693e95aa1502e62d31304ebec2dc49c9521
ifBeans <fabiandustin27@gmail.com>
2024-10-24 21:28:09 +0700

    Test

--- index.php
+++ index.php
@@ -42,7 +42,7 @@ $pstatement4->execute();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login</title>
     <link href="./src/output.css" rel="stylesheet">
 </head>
 <body>
